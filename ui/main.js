057002b6bad6b470bad0f5965bfc154c1617fd49
aakankshax <aakanksha.bc@gmail.com>
2017-08-21 16:08:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,14 @@
 // counter code
-var button = documnet.getElementById('counter');
+var button = document.getElementById('counter');
 var counter=0;
-button.onclick() = function() {
+button.onclick = function() {
   // Make a request to the counter endpoint
   
   // Capture the response and store it in a variable
   
   // Render the variable in the correct span
   counter = counter+1;
-  var span = documnet.getElementById('count');
+  var span = document.getElementById('count');
   span.innerHTML = counter.toString();
   
   
