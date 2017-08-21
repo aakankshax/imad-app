f40c974356745dbdd81ed5efe978ebdef9769d22
aakankshax <aakanksha.bc@gmail.com>
2017-08-21 15:14:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,11 @@ element.innerHTML = 'New value';
 // Move the image
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft+10;
+    madi.style.marginLeft = marginLeft+'px';
+}
 madi.onclick = function() { 
-    madi.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 };
